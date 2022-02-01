b363a973503552af8dfd3caab8340aba0bbdbc40
Kaouthar20 <kaouthar.elouatri@gmail.com>
2022-02-01 10:40:50 +0100

    Ading control_Access

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,26 +9,4 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('etudiant_liste');
-        }
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $username = $authenticationUtils->getLastUsername();
-
-        return $this->render('login/login.html.twig', [
-            'username' => $username,
-            'error'         => $error,
-        ]);
-    }
-    /**
-     * @Route("/logout", name="logout", methods={"GET"})
-     */
-    public function logout()
-    {
-    }
 }
